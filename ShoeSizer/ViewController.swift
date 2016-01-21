4fa93c6ef649d27a98f5b1e9b07742eac001e9c8
Agrim Singh <agrim.singh@gmail.com>
2016-01-21 11:31:10 +0800

    ShoeSizer
    First app in Swift

--- ShoeSizer/ViewController.swift
+++ ShoeSizer/ViewController.swift
@@ -20,6 +20,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
 
+    @IBOutlet weak var womenShoeSizeTextField: UITextField!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+
+    @IBAction func menButtonPressed(sender: UIButton) {
+        let men = Double(mensShoeSizeTextField.text!)
+        let menconv = men! + 30
+        messageLabel.text = "\(menconv)"
+    }
+
+    @IBAction func womenButtonPressed(sender: UIButton) {
+        let women = Double(womenShoeSizeTextField.text!)
+        let womenconv = women! + 30.5
+        messageLabel.text = "\(womenconv)"
+    }
+    
+    @IBAction func resetButtonPressed(sender: UIButton) {
+        messageLabel.text = ""
+        
+    }
+    
+    
 }
 
